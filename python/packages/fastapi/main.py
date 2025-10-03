3a59d8108ae2cf5e4cb4877f48a522fb7fbc8186
ShengJie <shengjie@bytedance.com>
2025-10-03 18:49:17 +0800

    init fastapi

--- python/packages/fastapi/main.py
+++ python/packages/fastapi/main.py
@@ -1,1 +1,14 @@
-print('hello')
+from typing import Union
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root() -> dict[str, str]:
+    return {"Hello": "Worldaa"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
