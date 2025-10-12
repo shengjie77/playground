1fac570272e93a978f9765ba6c954a8ad9155fcf
ShengJie <shengjie0202@outlook.com>
2025-10-12 23:01:55 +0800

    async

--- rust/crates/lr-async/src/main.rs
+++ rust/crates/lr-async/src/main.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use trpl::Html;
 
 async fn page_title(url: &str) -> Option<String> {
@@ -8,13 +10,45 @@ async fn page_title(url: &str) -> Option<String> {
         .map(|title| title.inner_html())
 }
 
+async fn count() {
+    let handle = trpl::spawn_task(async {
+        for i in 1..10 {
+            println!("hi number {i} from the first task!");
+            trpl::sleep(Duration::from_millis(500)).await;
+        }
+    });
+
+    for i in 1..5 {
+        println!("hi number {i} from the second task!");
+        trpl::sleep(Duration::from_millis(500)).await;
+    }
+
+    handle.await.unwrap()
+}
+
+async fn message() {
+    let (tx, mut rx) = trpl::channel();
+    let vals = vec![
+        String::from("hi"),
+        String::from("from"),
+        String::from("the"),
+        String::from("future"),
+    ];
+
+    for val in vals {
+        println!("sending {val}");
+        tx.send(val).unwrap();
+        trpl::sleep(Duration::from_millis(500)).await;
+    }
+
+    while let Some(value) = rx.recv().await {
+        println!("received {value}");
+    }
+}
+
 
 fn main() {
     trpl::run(async {
-        let url = "https://www.rust-lang.org";
-        match page_title(url).await {
-            Some(title) => println!("The title of {url} was {title}"),
-            None => println!("{url} had no title")
-        }
+        message().await;
     })
 }
