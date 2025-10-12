a42a604ac7874988937a844c6c206f7d0816dbd9
ShengJie <shengjie0202@outlook.com>
2025-10-12 19:41:03 +0800

    init

--- rust/crates/lr-async/src/main.rs
+++ rust/crates/lr-async/src/main.rs
@@ -1,5 +1,20 @@
+use trpl::Html;
 
-fn main() {
-    println!("Hello, world!");
+async fn page_title(url: &str) -> Option<String> {
+    let response = trpl::get(url).await;
+    let response_text = response.text().await;
+    Html::parse(&response_text)
+        .select_first("title")
+        .map(|title| title.inner_html())
 }
 
+
+fn main() {
+    trpl::run(async {
+        let url = "https://www.rust-lang.org";
+        match page_title(url).await {
+            Some(title) => println!("The title of {url} was {title}"),
+            None => println!("{url} had no title")
+        }
+    })
+}
